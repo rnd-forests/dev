240b028026ee269d27bc9730312c1f9526f873d5
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2019-09-09 15:30:52 +0700

    Create config.fish

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+# Greeting when start
+set -g -x fish_greeting 'Hi, Fly!'
+
+# fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
