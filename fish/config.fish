6c06aae202610524e03b7bef6d4b13676864e26d
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2019-09-10 08:14:53 +0700

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -32,7 +32,7 @@ set -g theme_color_scheme dark
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
-set -g theme_newline_prompt '> '
+set -g theme_newline_prompt '$ '
 
 alias pbpaste='xsel --clipboard --output'
 alias pbcopy='xsel --clipboard --input && notify_contents_copied'
